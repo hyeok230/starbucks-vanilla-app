3789c07716d9d4ab44b6a4b42472344c78b01e47
hyeok230 <hyeok230@naver.com>
2023-04-25 21:57:51 +0900

    feat: fixed image & 3D animation

--- README.md
+++ README.md
@@ -344,3 +344,14 @@ function onYouTubePlayerAPIReady() {
   });
 }
 ```
+
+## 랜덤한 숫자를 생성하는 함수
+
+```js
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+```
--- js/main.js
+++ js/main.js
@@ -75,3 +75,26 @@ promotionToggleBtn.addEventListener('click', function () {
         promotionEl.classList.remove('hide');
     }
 });
+
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+}
+function floatingObject(selector, delay, size) {
+    // gsap.to(요소, 시간, 옵션);
+    gsap.to(
+        selector, // 선택자
+        random(1.5, 2.5), // 애니메이션 동작 시간
+        { // 옵션
+            y: size, // 위아래로 움직일 사이즈
+            repeat: -1, // 무한 반복
+            yoyo: true, // 자연스럽게 역재생
+            ease: Power1.easeInOut, // Easing 함수 적용
+            delay: random(0, delay)
+        }
+    );
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
